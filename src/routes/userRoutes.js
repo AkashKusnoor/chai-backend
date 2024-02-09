43200a722e09d444c4d7061c06a8e43349927738
Akash Kusnoor <akashkusnoor005@gmail.com>
2024-02-09 12:38:38 +0530

    add new routes for user controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { loginUser, logoutUser, refreshAccessToken, reigisterUser } from "../controllers/userController.js";
+import { changecurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, reigisterUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/userController.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
+import multer from "multer";
 
 const router =express.Router()
 //register
@@ -22,8 +23,15 @@ router.post("/login",loginUser);
 
 //secured routes
 //logout
-router.post("/logout",verifyJWT,logoutUser);
-router.post("/refresh-token", refreshAccessToken)
+router.post("/logout", verifyJWT,logoutUser);
+router.post("/refresh-token", refreshAccessToken);
+router.post("/change-password", verifyJWT, changecurrentPassword);
+router.post("/current-user", verifyJWT, getCurrentUser);
+router.patch("/update-account", verifyJWT, updateAccountDetails);
+router.patch("/update-avatar", verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.patch("/update-coverImage", verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.get("/channel/:username", verifyJWT, getUserChannelProfile);
+router.get("/watcHistory/:", verifyJWT, getWatchHistory)
 
 
 export  default router;
